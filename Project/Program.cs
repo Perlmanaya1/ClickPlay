cfee1d3c3874b04bfa83116f4aba12f0edf82a97
Perlmanaya1 <ayala57194@gmail.com>
2022-11-24 00:37:23 +0200

    the full project

---
+++ Project/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Algoritem;
+using NFugue.Midi;
+using NFugue.Patterns;
+using NFugue.Playing;
+using NFugue.Theory;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Algoritem.Algoritem algoritem = new Algoritem.Algoritem();
+            algoritem.exec();
+            Console.Read();
+
+        }
+    }
+}
